1edcb40e593eeda3a973ad0b2617a2225a4818d6
JoeLee1994 <li.zhuo@husky.neu.edu>
2018-12-13 17:21:46 -0500

    Update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -4,9 +4,9 @@ mongoose.Promise = require('q').Promise;
 let connectionString = 'mongodb://localhost/db-project'; // for local
 
 
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    let username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-    let password = process.env.MLAB_PASSWORD_WEBDEV;
+if(process.env.MLAB_USERNAME_CS5200) { // check if running remotely
+    let username = process.env.MLAB_USERNAME_CS5200; // get from environment
+    let password = process.env.MLAB_PASSWORD_CS5200;
 
     connectionString = 'mongodb://' + username + ':' + password;
     connectionString += '@ds117250.mlab.com:17250/heroku_rbdgfw80';
